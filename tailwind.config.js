ea1e68a4a8e3f06f5f9ecd8450b866f4ebb467b8
Vader1970 <daniel.wilkey@gmail.com>
2024-05-06 13:59:40 +1200

    Talwind Config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{jsx,tsx}", "./components/**/*.{jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        // Over rides default font
         sans: ["var(--font-exo2)", "sans-serif"],
-        // add own font
         orbitron: ["var(--font-orbitron)", "sans-serif"],
       },
     },
