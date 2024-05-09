39855b57e544762c37d7b67382a1c38bc23463a9
Vader1970 <daniel.wilkey@gmail.com>
2024-05-09 15:06:57 +1200

    Home Page Comments added

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,30 @@ import Heading from "../components/Heading";
 import { getReviews } from "../lib/reviews";
 import Link from "next/link";
 
-// export const dynamic = "force-dynamic";
-// export const revalidate = 30; //30 seconds
-
+// Home Page component
 export default async function HomePage() {
+  // Fetching the latest 3 reviews from the API
   const { reviews } = await getReviews(3);
-  // console.log("[HomePage] rendering:", reviews.map((review) => review.slug).join(", "));
-  // Next.js renders HTML on the server
-  // console.log("[Homepage] rendering");
+
+  // Render the Home Page
   return (
     <>
+      {/* Page Heading */}
       <Heading>Indie Gamer</Heading>
+      {/* Page Description */}
       <p className='pb-3'>Only the best indie games, reviewed for you.</p>
+      {/* List of Reviews */}
       <ul className='flex flex-col gap-3'>
+        {/* Mapping through each review */}
         {reviews.map((review, index) => (
           <li
             key={review.slug}
             className='bg-white border rounded shadow w-80
                        hover:shadow-xl sm:w-full'
           >
+            {/* Link to Review Page */}
             <Link href={`/reviews/${review.slug}`} className='flex flex-col sm:flex-row'>
+              {/* Review Image */}
               <Image
                 src={review.image}
                 alt=''
@@ -31,8 +35,11 @@ export default async function HomePage() {
                 height='180'
                 className='rounded-t sm:rounded-l sm:rounded-r-none'
               />
+              {/* Review Details */}
               <div className='px-2 py-1 text-center sm:text-left'>
+                {/* Review Title */}
                 <h2 className='font-orbitron font-semibold'>{review.title}</h2>
+                {/* Review Subtitle */}
                 <p className='hidden pt-2 sm:block'>{review.subtitle}</p>
               </div>
             </Link>
